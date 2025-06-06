cdf58bb8a4a0591f8bbecfa2ee13d41d1f6965d3
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-06-06 14:50:35 +0500

    asdas

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express'
 import 'dotenv/config'
 import cor from 'cors'
 import { connect } from './src/db/connect.js'
-import route from './src/router/router.js'
-import authRoute from './src/router/auth.route.js'
+import route from './src/api/router.js'
+import authRoute from './src/api/auth.route.js'
 import cookieParser from 'cookie-parser'
 
 
