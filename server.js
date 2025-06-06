2c9117cc1dc38932c990c8efc46087d4a736f851
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-06-06 16:33:55 +0500

    sadasd

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ import cookieParser from 'cookie-parser'
 const app = express()
 app.use(express.json({ limit: "20mb" }))
 app.use(express.urlencoded({ limit: "20mb", extended: true }))
-app.use(cor())
+app.use(cor(
+    {
+        origin: ["http://localhost:8000","https://ai-blogs.up.railway.app/"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }
+))
 app.use(cookieParser())
 app.use('/api/v2', route)
 app.use('/api/v1', authRoute)
