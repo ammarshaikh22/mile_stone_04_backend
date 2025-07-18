ecd8386be027f925dcda253bd8f116a29bd78714
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-07-18 00:01:13 -0700

    dsfsdf

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -58,7 +58,7 @@ export const Login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 24 * 60 * 60 * 1000
     });
     user.isLogin = true;
